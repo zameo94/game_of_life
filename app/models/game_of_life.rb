370cd6b5457247799173cf5ec0e7ac0be77d0bbb
matteo <matdeliperi.md@gmail.com>
2022-03-16 01:16:50 +0100

    dev.ing upload_txt, add test

--- app/models/game_of_life.rb
+++ app/models/game_of_life.rb
@@ -1,9 +1,27 @@
 class GameOfLife
   attr_accessor :width
   attr_accessor :height
+  attr_accessor :file
 
   def initialize
     self.width = 10
     self.height = 10
+  end
+
+  def upload_txt(path = "storage/test.txt")
+    validate_file(File.open(path, "r"))
+  end
+
+  private
+
+  def validate_file(file)
+    if File.extname(file).eql? ".txt"
+      self.file = file
+      puts self.file.read
+      true
+    else
+      puts "Error format"
+      false
+    end
   end
 end
--- spec/models/game_of_life_spec.rb
+++ spec/models/game_of_life_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GameOfLife, type: :model do
   let(:game_of_life) { GameOfLife.new }
 
-  context "test game od life rule" do
+  context "GameOfLife" do
     it "must have width & height" do
       expect(game_of_life.width.present?).to eq(true)
       expect(game_of_life.height.present?).to eq(true)
@@ -12,6 +12,13 @@ RSpec.describe GameOfLife, type: :model do
     it "10 as default width & height" do
       expect(game_of_life.width).to eq(10)
       expect(game_of_life.height).to eq(10)
+    end
+  end
+
+  context "upload_txt" do
+    it "accept only txt" do
+      expect(game_of_life.upload_txt("storage/test.txt")).to eq(true)
+      expect(game_of_life.upload_txt("storage/test.xml")).to eq(false)
     end
   end
 end
