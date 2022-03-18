63ac9f3d42ff13e84296db4c17fadf153a7b136c
matteo <matdeliperi.md@gmail.com>
2022-03-18 23:49:41 +0100

    improve test, improve code, improve error system

--- app/models/game_of_life.rb
+++ app/models/game_of_life.rb
@@ -7,37 +7,21 @@ class GameOfLife
   attr_accessor :frame
 
   def initialize
-    self.width = 10
-    self.height = 10
+    self.width = 0
+    self.height = 0
+    self.error = ''
     @alive_cells = {}
   end
 
   def run(path = "storage/test.txt")
+    @alive_cells = {}
+    self.error = ''
     upload_txt(path)
 
-    if error.nil?
-      print_frame
-
-      advance_frame
-      true
-    end
-  end
-
-  def print_frame
-    puts "Generation: #{self.generation}"
-    puts "#{self.height} #{self.width}"
-
-    self.height.times.each do |y|
-      output = ""
-      self.width.times.each do |x|
-        if @alive_cells.has_key?([y , x])
-          output += "*"
-        else
-          output += "."
-        end
-      end
-      puts output
-    end
+    return self.error unless self.error.empty?
+    print_frame
+    sleep 0.5
+    advance_frame
   end
 
   def upload_txt(path)
@@ -50,39 +34,33 @@ class GameOfLife
         parse_file
       end
     else
-      self.error = "File not found"
+      self.error += "File not found. "
+    end
+  end
+
+  def print_frame
+    puts "Generation: #{self.generation}"
+    puts "#{self.height} #{self.width}"
+
+    self.height.times.each do |y|
+      output = ""
+      self.width.times.each do |x|
+        if @alive_cells.has_key?([y, x])
+          output += "*"
+        else
+          output += "."
+        end
+      end
+      puts output
     end
   end
 
   def advance_frame
-    new_alive = {}
-    self.height.times do |y|
-      self.width.times do |x|
-        alive = self.frame[y][x].eql? "*"
-
-        alive_neightbours = [
-          @alive_cells.has_key?([y - 1, x - 1]), # Top Left
-          @alive_cells.has_key?([y - 1, x]), # Top
-          @alive_cells.has_key?([y - 1, x + 1]), # Top Right
-          @alive_cells.has_key?([y, x + 1]), # Right
-          @alive_cells.has_key?([y + 1, x + 1]), # Bottom Right
-          @alive_cells.has_key?([y + 1, x]), # Bottom
-          @alive_cells.has_key?([y + 1, x - 1]), # Bottom Left
-          @alive_cells.has_key?([y, x - 1]) # Left
-        ].count(true)
-
-        if (alive and alive_neightbours.between?(2, 3)) or (!alive and alive_neightbours.eql? 3)
-          new_alive[[y, x]] = true
-        end
-      end
-    end
-
-    @alive_cells = new_alive
-
+    recalculate_alive_cells
     self.generation += 1
 
     puts ""
-    puts "-"*50
+    puts "-" * 50
     puts ""
     print_frame
     sleep 0.5
@@ -99,16 +77,15 @@ class GameOfLife
     if File.extname(file).eql? ".txt"
       self.file = file.read
     else
-      self.error = "Wrong file format"
+      self.error += "Wrong file format. "
     end
   end
 
   def parse_file
     @rows = self.file.split(/\n/)
     get_generation
-    get_width
-    get_height
-    get_frame
+    get_width_height
+    get_frame if self.error.empty?
   end
 
   def get_generation
@@ -116,28 +93,35 @@ class GameOfLife
     if row.include? "Generation "
       columns = row.split(":")
       self.generation = columns[0].last.to_i
+    else
+      self.error += "Unable to find generation. "
     end
   end
 
-  def get_width
+  def get_width_height
     row = @rows[1]
-    self.width = row[2].to_i
-    self.error = 'Unable to get with' if self.width.nil?
-  end
 
-  def get_height
-    row = @rows[1]
-    self.height = row[0].to_i
-    self.error = 'Unable to get height' if self.height.nil?
+    if row.nil?
+      self.error += "Unable to get widht & height. "
+    else
+      if (row[0].to_i > 0) and (row[2].to_i > 0)
+        self.width = row[2].to_i
+        self.height = row[0].to_i
+      else
+        self.error += "Unable to get widht & height. "
+      end
+    end
   end
 
   def get_frame
-    frame = Array.new(self.height) {Array.new(self.width)}
+    frame = Array.new(self.height) { Array.new(self.width) }
 
     self.height.times do |y|
       self.width.times do |x|
+        # sum y + 2 for compensate the first & second file's lines
         frame[y][x] = @rows[y + 2][x]
 
+        # @alice_cells contain the coordination of all live cells, used to advance the frame
         if @rows[y + 2][x].eql? "*"
           @alive_cells[[y, x]] = true
         end
@@ -145,5 +129,33 @@ class GameOfLife
     end
 
     self.frame = frame
+    self.error += "Unable to get frame\n" if self.frame.nil?
+  end
+
+  def recalculate_alive_cells
+    new_alive = {}
+    self.height.times do |y|
+      self.width.times do |x|
+        alive = self.frame[y][x].eql? "*"
+
+        alive_neightbours = [
+          @alive_cells.has_key?([y - 1, x - 1]), # Top Left
+          @alive_cells.has_key?([y - 1, x]), # Top
+          @alive_cells.has_key?([y - 1, x + 1]), # Top Right
+          @alive_cells.has_key?([y, x + 1]), # Right
+          @alive_cells.has_key?([y + 1, x + 1]), # Bottom Right
+          @alive_cells.has_key?([y + 1, x]), # Bottom
+          @alive_cells.has_key?([y + 1, x - 1]), # Bottom Left
+          @alive_cells.has_key?([y, x - 1]) # Left
+        ].count(true)
+
+        #Here it apply the Game of life rules for recalculate all the @alive_cells
+        if (alive and alive_neightbours.between?(2, 3)) or (!alive and alive_neightbours.eql? 3)
+          new_alive[[y, x]] = true
+        end
+      end
+    end
+
+    @alive_cells = new_alive
   end
 end
--- spec/models/game_of_life_spec.rb
+++ spec/models/game_of_life_spec.rb
@@ -11,25 +11,25 @@ RSpec.describe GameOfLife, type: :model do
     end
 
     it "10 as default width & height" do
-      expect(game_of_life.width).to eq(10)
-      expect(game_of_life.height).to eq(10)
+      expect(game_of_life.width).to eq(0)
+      expect(game_of_life.height).to eq(0)
     end
   end
 
   context "upload_txt" do
     it "upload txt" do
       game_of_life.upload_txt(path)
-      expect(game_of_life.error).to eq(nil)
+      expect(game_of_life.error).to eq('')
     end
 
     it "upload not txt" do
-      game_of_life.upload_txt("storage/test.xml")
-      expect(game_of_life.error).to eq("Wrong file format")
+      game_of_life.upload_txt("spec/files/test.xml")
+      expect(game_of_life.error).to eq("Wrong file format. ")
     end
 
     it "file not found" do
       game_of_life.upload_txt("storage/test.aaa")
-      expect(game_of_life.error).to eq("File not found")
+      expect(game_of_life.error).to eq("File not found. ")
     end
 
     it "self.file" do
@@ -42,40 +42,40 @@ RSpec.describe GameOfLife, type: :model do
 
   context "sample.txt" do
     it "self.generation" do
-      game_of_life.upload_txt("storage/sample.txt")
+      game_of_life.upload_txt(path)
 
       expect(game_of_life.generation).not_to eq(nil)
     end
 
     it "get_generation" do
       my_generation = 3
-      game_of_life.upload_txt("storage/sample.txt")
+      game_of_life.upload_txt(path)
 
       expect(game_of_life.generation).to eq(my_generation)
     end
 
     it "get_width" do
       my_width = 8
-      game_of_life.upload_txt("storage/sample.txt")
+      game_of_life.upload_txt(path)
 
       expect(game_of_life.width).to eq(my_width)
-      expect(game_of_life.error).to eq(nil)
+      expect(game_of_life.error).to eq("")
     end
 
     it "get_height" do
       my_height = 4
-      game_of_life.upload_txt("storage/sample.txt")
+      game_of_life.upload_txt(path)
 
       expect(game_of_life.height).to eq(my_height)
-      expect(game_of_life.error).to eq(nil)
+      expect(game_of_life.error).to eq("")
     end
 
     it "get_frame" do
       my_frame = [%w[. . . . . . . .], %w[. . . . * . . .], %w[. . . * * . . .], %w[. . . . . . . .]]
-      game_of_life.upload_txt("storage/sample.txt")
+      game_of_life.upload_txt(path)
 
       expect(game_of_life.frame).to eq(my_frame)
-      expect(game_of_life.error).to eq(nil)
+      expect(game_of_life.error).to eq("")
     end
   end
 end
