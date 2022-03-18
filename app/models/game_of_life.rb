d8b8d265fcb18e854766f1394d00be017f1fb858
matteo <matdeliperi.md@gmail.com>
2022-03-18 17:34:45 +0100

    improve validation, add parse_file, add get_generation, get_width, get_height, get_frame. Add and improve test, add self.frame and print it on console

--- app/models/game_of_life.rb
+++ app/models/game_of_life.rb
@@ -2,31 +2,93 @@ class GameOfLife
   attr_accessor :width
   attr_accessor :height
   attr_accessor :file
+  attr_accessor :generation
   attr_accessor :error
+  attr_accessor :frame
 
   def initialize
     self.width = 10
     self.height = 10
   end
 
-  def upload_txt(path = "storage/test.txt")
+  def run(path = "storage/test.txt")
+    upload_txt(path)
+
+    if error.nil?
+      puts "Generation: #{self.generation}"
+      puts "#{self.height} #{self.width}"
+      puts "#{self.frame}"
+    end
+  end
+
+  def upload_txt(path)
     if File.exist?(path)
-      validate_file(File.open(path, "r"))
+      file = File.open(path, "r")
+      validate_file(file)
+      file.close
+
+      unless self.file.nil?
+        parse_file
+      end
     else
       self.error = "File not found"
-      false
     end
   end
 
   private
 
   def validate_file(file)
+    validate_extension(file)
+  end
+
+  def validate_extension(file)
     if File.extname(file).eql? ".txt"
-      self.file = file
-      true
+      self.file = file.read
     else
       self.error = "Wrong file format"
-      false
     end
+  end
+
+  def parse_file
+    @rows = self.file.split(/\n/)
+    get_generation
+    get_width
+    get_height
+    get_frame
+  end
+
+  def get_generation
+    row = @rows[0]
+    if row.include? "Generation "
+      columns = row.split(":")
+      self.generation = columns[0].last.to_i
+    end
+  end
+
+  def get_width
+    row = @rows[1]
+    self.width = row[2].to_i
+    self.error = 'Unable to get with' if self.width.nil?
+  end
+
+  def get_height
+    row = @rows[1]
+    self.height = row[0].to_i
+    self.error = 'Unable to get height' if self.height.nil?
+  end
+
+  def get_frame
+    frame = ""
+    for i in 0...self.height
+      #I sum 2 at row index 'i' for exclude the generation and width/height rows
+      frame += @rows[i+2]
+
+      #knowning which arrays start at 0, i add 1 at row index 'i' for ri-equilibrate counts,
+      # so the \n is not added at the last line
+      frame += "\n" unless (i + 1).eql? self.height
+    end
+
+    self.frame = frame
+    self.error = 'Unable to get frame' if self.frame.nil?
   end
 end
--- spec/models/game_of_life_spec.rb
+++ spec/models/game_of_life_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe GameOfLife, type: :model do
-  let!(:game_of_life) { GameOfLife.new }
-  let!(:path) { "storage/test.txt" }
+  let(:game_of_life) { GameOfLife.new }
+  let!(:path) { "storage/sample.txt" }
 
   context "GameOfLife" do
     it "must have width & height" do
@@ -18,17 +18,17 @@ RSpec.describe GameOfLife, type: :model do
 
   context "upload_txt" do
     it "upload txt" do
-      expect(game_of_life.upload_txt(path)).to eq(true)
+      game_of_life.upload_txt(path)
       expect(game_of_life.error).to eq(nil)
     end
 
     it "upload not txt" do
-      expect(game_of_life.upload_txt("storage/test.xml")).to eq(false)
+      game_of_life.upload_txt("storage/test.xml")
       expect(game_of_life.error).to eq("Wrong file format")
     end
 
     it "file not found" do
-      expect(game_of_life.upload_txt("storage/test.aaa")).to eq(false)
+      game_of_life.upload_txt("storage/test.aaa")
       expect(game_of_life.error).to eq("File not found")
     end
 
@@ -36,7 +36,46 @@ RSpec.describe GameOfLife, type: :model do
       game_of_life.upload_txt(path)
 
       expect(game_of_life.file).not_to eq(nil)
-      expect(game_of_life.file.read).to eq("Ciaoooooooooooooo")
+      expect(game_of_life.file).to eq("Generation 3:\n4 8\n........\n....*...\n...**...\n........")
+    end
+  end
+
+  context "sample.txt" do
+    it "self.generation" do
+      game_of_life.upload_txt("storage/sample.txt")
+
+      expect(game_of_life.generation).not_to eq(nil)
+    end
+
+    it "get_generation" do
+      my_generation = 3
+      game_of_life.upload_txt("storage/sample.txt")
+
+      expect(game_of_life.generation).to eq(my_generation)
+    end
+
+    it "get_width" do
+      my_width = 8
+      game_of_life.upload_txt("storage/sample.txt")
+
+      expect(game_of_life.width).to eq(my_width)
+      expect(game_of_life.error).to eq(nil)
+    end
+
+    it "get_height" do
+      my_height = 4
+      game_of_life.upload_txt("storage/sample.txt")
+
+      expect(game_of_life.height).to eq(my_height)
+      expect(game_of_life.error).to eq(nil)
+    end
+
+    it "get_frame" do
+      my_frame = "........\n....*...\n...**...\n........"
+      game_of_life.upload_txt("storage/sample.txt")
+
+      expect(game_of_life.frame).to eq(my_frame)
+      expect(game_of_life.error).to eq(nil)
     end
   end
 end
